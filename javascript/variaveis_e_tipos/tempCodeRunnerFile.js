1eb3a6124f008cd76b7f293e38621e8a9086fe76
Renan Dos Santos Lopes <renan.lop@hotmail.com>
2022-06-10 22:00:30 -0300

    Complemento de título nas atividades 'variaveis_e_tipos'

--- javascript/variaveis_e_tipos/tempCodeRunnerFile.js
+++ javascript/variaveis_e_tipos/tempCodeRunnerFile.js
@@ -1,3 +1,5 @@
+// Função que substitui os números pares por 0, e caso o array base esteja vazio (ou null, ou undefined), é retornado -1.
+
 let array_base = [1, 2, 3, 4, 5, 6, 343, 54464, 343, 2325, 768, 86878, 4543, 2323, 12232, 4657, 455768, 96, 658, 9]
 function trocaPares(valor, indice, array) {
     if (array) {
