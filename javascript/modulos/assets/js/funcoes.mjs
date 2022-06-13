6dc95aa5ce5055da2dea7ebd63f4cfab97de301e
Renan Lopes <renan.lop@hotmail.com>
2022-06-12 21:28:12 -0300

    Exercicios sobre modulos

---
+++ javascript/modulos/assets/js/funcoes.mjs
@@ -0,0 +1,9 @@
+let mostraIdade = (nome, idade) => console.log(`Seu nome é ${nome}, e sua idade é ${idade}`);
+let mostraCidade = (cidade) => console.log(`Sua cidade é ${cidade}`);
+let mostraHobby = (hobby) => console.log(`Seu hobby é ${hobby}`);
+
+export {
+    mostraIdade,
+    mostraCidade,
+    mostraHobby,
+}
---
+++ javascript/modulos/assets/js/main.mjs
@@ -0,0 +1,9 @@
+import {
+    mostraCidade,
+    mostraHobby,
+    mostraIdade
+} from './funcoes.mjs'
+
+mostraCidade("Rio de Janeiro");
+mostraHobby("Tenis de mesa");
+mostraIdade("Renan", 35);
